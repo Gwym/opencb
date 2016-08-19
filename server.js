8d64f5ed7e99a02223898d836b50fa72bc31419a
Gwym Hendawyr <gwym.hendawyr@gmail.com>
2016-08-19 11:08:43 +0200

    Websocket server creation

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var fs = require('fs');
 var http = require('http');
 var WebSocketServer = require('ws').Server;
 
-console.log(WebSocketServer);
+
+//  Set the constants we need.
+var env = {
+	protocol: 'yh001'
+}
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -36,6 +41,23 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     console.warn('MongoDB configuration error ' + mongoHost + ' ' + mongoPort + ' ' + mongoDatabase);
   }
 }
+
+
+var toStr = function (o) {
+	var o_str = '';
+	for (var k in o) {
+		// o_str += typeof o[k] + ' \n';
+		if (typeof o[k] === 'function') {
+			// o_str += k + ' : ' + ('' + o[k]).substring(0,10) + ' \n';
+		}
+		else {
+			o_str += k + ' : ' + o[k] + ' \n';
+		}
+	}
+	return o_str;
+}
+
+
 var db = null,
   dbDetails = new Object();
 
@@ -76,6 +98,91 @@ var server = http.createServer(function (req, res) {
 }).listen(port, ip);
 console.log('%s: Node server started on %s:%d',
   Date(Date.now()), ip, port);
+
+
+
+// cf. OPENSHIFT doc : plain WebSockets ws:// will use port 8000 and secured connections wss:// port 8443
+
+
+var wss = new WebSocketServer(
+  { server: server }
+  , function () {
+    console.log('WS > listen callback ');
+  });
+
+// TODO (0) : hash passwords, rolling id, mongo datastore
+var datastore = {
+  user: {
+    test1: {
+      pwd: 'test1',
+      name: 'Test1'
+    }
+  }
+}
+
+wss.on('connection', function (ws) {
+
+  if (ws.protocol !== env.protocol) {
+    console.log('WS > bad protocol, closing ' + toStr(ws));
+
+    ws.close();
+    return;
+  }
+
+  // console.log('WS > connection ' + toStr(ws));
+  console.log('WS > clients ' + wss.clients.length); // + ' ' + toStr(wss.clients[0]));
+
+  ws.once('message', function (message) {
+
+    console.log('WS auth > received: %s', message);
+
+    var u;
+
+    try {
+
+      u = JSON.parse(message);
+
+      console.log(u);
+
+      // check user id/pwd
+      if (u.seq === 0 && u.pwd === datastore.user[u.id].pwd) {
+
+        ws.userID = u.id;
+        ws.on('message', function (message) {
+
+          try {
+
+            u = JSON.parse(message);
+            console.log('WS > received: %s on %s', u, ws.userID);
+            // dispatch(user[ws.userID]) or ws.dispatch( ?=)
+
+          }
+          catch (e) {
+            console.log('WS > error ' + e + ', closing ' + toStr(ws));
+            ws.close();
+          }
+
+        });
+
+        console.log('User  ' + u.id + ' connected');
+        ws.send('welcome ' + datastore.user[u.id].name);
+
+      }
+      else {
+        console.log('WS > wrong id or password ' + toStr(u) + ', closing ' + toStr(ws));
+        ws.close();
+      }
+
+    }
+    catch (e) {
+      console.log('WS auth > error ' + e + ', closing ' + toStr(ws));
+      ws.close();
+    }
+
+  }); // auth once
+
+  ws.send('who?');
+});
 
 
 /*
