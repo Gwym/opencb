7a6bb3ff6490229f89396ef1d136aa0974a8d0aa
Gwym Hendawyr <gwym.hendawyr@gmail.com>
2016-08-29 12:35:45 +0200

    Conversion to Typescript

--- server.js
+++ server.js
@@ -1,237 +1,139 @@
-"use strict"
-
-var fs = require('fs');
+"use strict";
 var http = require('http');
-var WebSocketServer = require('ws').Server;
-
-
-//  Set the constants we need.
-var env = {
-	protocol: 'yh001'
-}
-
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-  mongoURLLabel = "";
-
-console.log('mongoURL :' + mongoURL + ' ' + process.env.DATABASE_SERVICE_NAME);
-console.log('server :' + ip + ' ' + port);
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
-    mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-  }
-  else {
-    console.warn('MongoDB configuration error ' + mongoHost + ' ' + mongoPort + ' ' + mongoDatabase);
-  }
-}
-
-
-var toStr = function (o) {
-	var o_str = '';
-	for (var k in o) {
-		// o_str += typeof o[k] + ' \n';
-		if (typeof o[k] === 'function') {
-			// o_str += k + ' : ' + ('' + o[k]).substring(0,10) + ' \n';
-		}
-		else {
-			o_str += k + ' : ' + o[k] + ' \n';
-		}
-	}
-	return o_str;
-}
-
-
-var db = null,
-  dbDetails = new Object();
-
-var initDb = function (callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function (err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+var ws = require('ws');
+var fileServer = require('node-static');
+var mongodb = require('mongodb');
+let WebSocketServer = ws.Server;
+let MongoClient = mongodb.MongoClient;
+let env = {
+    protocol: 'ocb1',
+    ipaddress: process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    mongoURL: process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel: ''
 };
-
-
-initDb(function (err) {
-  console.log('Error connecting to Mongo. Message:\n' + err);
-});
-
-// index page
-var html = fs.readFileSync('views/index.html');
-
-var server = http.createServer(function (req, res) {
-
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(html);
-
-}).listen(port, ip);
-console.log('%s: Node server started on %s:%d',
-  Date(Date.now()), ip, port);
-
-
-
-// cf. OPENSHIFT doc : plain WebSockets ws:// will use port 8000 and secured connections wss:// port 8443
-
-
-var wss = new WebSocketServer(
-  { server: server }
-  , function () {
-    console.log('WS > listen callback ');
-  });
-
-// TODO (0) : hash passwords, rolling id, mongo datastore
-var datastore = {
-  user: {
-    test1: {
-      pwd: 'test1',
-      name: 'Test1'
+var toStr = function (o) {
+    var o_str = '';
+    for (var k in o) {
+        if (typeof o[k] === 'function') {
+        }
+        else {
+            o_str += k + ' : ' + o[k] + ' \n';
+        }
     }
-  }
+    return o_str;
+};
+var file = new fileServer.Server('webapp', { indexFile: "index.html", cache: 0 });
+var logger = {
+    info: function (s) {
+        console.log(s);
+    },
+    error: function (err) {
+        console.error(err);
+    }
+};
+class User {
+    constructor(name) {
+        this.name = name;
+    }
 }
-
-wss.on('connection', function (ws) {
-
-  if (ws.protocol !== env.protocol) {
-    console.log('WS > bad protocol, closing ' + toStr(ws));
-
-    ws.close();
-    return;
-  }
-
-  // console.log('WS > connection ' + toStr(ws));
-  console.log('WS > clients ' + wss.clients.length); // + ' ' + toStr(wss.clients[0]));
-
-  ws.once('message', function (message) {
-
-    console.log('WS auth > received: %s', message);
-
-    var u;
-
-    try {
-
-      u = JSON.parse(message);
-
-      console.log(u);
-
-      // check user id/pwd
-      if (u.seq === 0 && u.pwd === datastore.user[u.id].pwd) {
-
-        ws.userID = u.id;
-        ws.on('message', function (message) {
-
-          try {
-
-            u = JSON.parse(message);
-            console.log('WS > received: %s on %s', u, ws.userID);
-            // dispatch(user[ws.userID]) or ws.dispatch( ?=)
-
-          }
-          catch (e) {
-            console.log('WS > error ' + e + ', closing ' + toStr(ws));
-            ws.close();
-          }
-
+var users = {
+    find: function (id) {
+        return new User('Test');
+    }
+};
+console.log('mongoURL :' + env.mongoURL + ' ' + process.env.DATABASE_SERVICE_NAME);
+console.log('server :' + env.ipaddress + ' ' + env.port);
+if (env.mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(), mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'], mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'], mongoDatabase = process.env[mongoServiceName + '_DATABASE'], mongoPassword = process.env[mongoServiceName + '_PASSWORD'], mongoUser = process.env[mongoServiceName + '_USER'];
+    if (mongoHost && mongoPort && mongoDatabase) {
+        env.mongoURLLabel = env.mongoURL = 'mongodb://';
+        if (mongoUser && mongoPassword) {
+            env.mongoURL += mongoUser + ':' + mongoPassword + '@';
+        }
+        env.mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+        env.mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    }
+    else {
+        console.warn('MongoDB configuration error ' + mongoHost + ' ' + mongoPort + ' ' + mongoDatabase);
+    }
+}
+var initDb = function (callback) {
+    if (env.mongoURL == null) {
+        console.error('MongoDB URL Error : null');
+        return;
+    }
+    MongoClient.connect(env.mongoURL, function (err, conn) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.log('Connected to MongoDB at: %s', env.mongoURL);
+    });
+};
+initDb(function (err) {
+    console.log('Error connecting to Mongo. Message:\n' + err);
+});
+var server = http.createServer((req, res) => {
+    req.headers.url = req.url;
+    req.headers.ip = req.socket.remoteAddress;
+    req.addListener('end', function () {
+        file.serve(req, res, function (err, result) {
+            if (err) {
+                logger.info(req.headers);
+                console.log(err);
+                console.log(result);
+                logger.error(err);
+                res.writeHead(err.status, err.headers);
+                res.end('<!DOCTYPE html><html><head><head><body>404 : ressource not found.</body></html>');
+            }
         });
-
-        console.log('User  ' + u.id + ' connected');
-        ws.send('welcome ' + datastore.user[u.id].name);
-
-      }
-      else {
-        console.log('WS > wrong id or password ' + toStr(u) + ', closing ' + toStr(ws));
+    }).resume();
+}).listen(env.port, env.ipaddress);
+console.log('%s: Node server started on %s:%d', Date(), env.ipaddress, env.port);
+let options = { server: server, clientTracking: true };
+var wss = new WebSocketServer(options, function () {
+    console.log('WS > listen callback ');
+});
+var ws_connection_counter = 0;
+wss.on('connection', function (ws) {
+    if (ws.protocol !== env.protocol) {
+        console.log('WS > bad protocol, closing ' + toStr(ws));
         ws.close();
-      }
-
+        return;
     }
-    catch (e) {
-      console.log('WS auth > error ' + e + ', closing ' + toStr(ws));
-      ws.close();
-    }
-
-  }); // auth once
-
-  ws.send('who?');
-});
-
-
-/*
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    console.log('WS > clients ' + wss.clients.length);
+    ws.once('message', function (message) {
+        console.log('WS auth > received: %s', message);
+        var u;
+        try {
+            u = JSON.parse(message);
+            console.log(u);
+            if (u.seq === 0 && u.pwd === users.find(u.login).password_hash) {
+                let userID = u.id;
+                ws.on('message', function (message) {
+                    try {
+                        u = JSON.parse(message);
+                        console.log('WS > received: %s on %s', u, userID);
+                    }
+                    catch (e) {
+                        console.log('WS > error ' + e + ', closing ' + toStr(ws));
+                        ws.close();
+                    }
+                });
+                console.log('User  ' + u.login + ' connected');
+                ws.send('welcome ' + users.find(u.login).name);
+            }
+            else {
+                console.log('WS > wrong id or password ' + toStr(u) + ', closing ' + toStr(ws));
+                ws.close();
+            }
+        }
+        catch (e) {
+            console.log('WS auth > error ' + e + ', closing ' + toStr(ws));
+            ws.close();
+        }
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+    ws.send('who?');
 });
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
-*/
+//# sourceMappingURL=server.js.map
